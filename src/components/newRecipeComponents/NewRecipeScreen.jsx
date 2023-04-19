d92d7c3d00746d67227dfd65c452359f3da451dd
Noah Clark <noahammon00@gmail.com>
2023-04-19 11:34:49 -0600

    added different font weights the the imported font family as well as created the form for adding a new recipe and some styling to said form

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import './NewRecipeScreen.css'
 
 const NewRecipeScreen = () => {
   return (
-    <section>
-      <h1>Tell us about your Recipe!</h1>
-      {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
+    <section className="new-rec-screen">
+      <form className="new-rec">
+        <h1 className="new-rec-h">Tell us about your Recipe!</h1>
+        <div className="horizontal-input">
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <div className="radio-div">
+          <input type="radio" name="type" id="cook"/>
+          <label htmlFor="cook">Cook</label>
+          <input type="radio" name="type" id="bake"/>
+          <label htmlFor="bake">Bake</label>
+          <input type="radio" name="type" id="drink"/>
+          <label htmlFor="drink">Drink</label>
+        </div>
+        <div className="horizontal-input">
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <form  className="new-ing">
+          <div className="form-display">
+            <div className="veritcal-input">
+              <input type="text" />
+              <input type="text" />
+            </div>
+            <ul>
+
+            </ul>
+          </div>
+
+          <button></button>
+        </form>
+        <textarea name="instruct" id="instruct" cols="30" rows="10"></textarea>
+        <button type="submit">Save</button>
+      </form>
     </section>
   );
 };
